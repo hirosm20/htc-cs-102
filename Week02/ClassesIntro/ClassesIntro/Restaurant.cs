549e4bc79c7bd90cc54f1fd903ce5325ab3b6af3
hirosm20 <marypat14green@gmail.com>
2019-02-13 19:31:19 -0500

    Restraunt Class

---
+++ Week02/ClassesIntro/ClassesIntro/Restaurant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClassesIntro
+{
+    class Restaurant
+    {
+        public string Food;
+        public int Money;
+        public string Name;
+
+        public void Eat()
+        {
+            MessageBox.Show("The food cost " + Money);
+        }
+
+        public void Bathroom()
+        {
+            MessageBox.Show("I need to go to the bathroom. Let's stop at " + Name);
+        }
+    }
+}
