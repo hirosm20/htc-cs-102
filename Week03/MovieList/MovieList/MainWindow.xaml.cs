647ec6fa93487b321db11ee056a302061222f5f4
hirosm20 <marypat14green@gmail.com>
2019-02-27 19:25:50 -0500

    Movie

--- Week03/MovieList/MovieList/MainWindow.xaml.cs
+++ Week03/MovieList/MovieList/MainWindow.xaml.cs
@@ -21,9 +21,39 @@ namespace MovieList
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Movie> MovieList;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            MovieList = new List<Movie>();
+        }
+
+
+
+        private void buttonAddMovie_Click(object sender, RoutedEventArgs e)
+        {
+            string name = textBoxTitle.Text;
+            int year = Convert.ToInt32(textBoxRelease.Text);
+            double tomatoes = Convert.ToDouble(textBoxTitle.Text);
+
+            Movie newMovie = new Movie(name, year,tomatoes);
+
+            MovieList.Add(newMovie); 
+
+            MessageBox.Show(name);
+            MessageBox.Show(Convert.ToString(year));
+        }
+
+        private void buttonShowMovie_Click(object sender, RoutedEventArgs e)
+        {
+
+            foreach (var movie in MovieList)
+            {
+                movie.ShowMovie();
+            }
+
         }
     }
 }
--- Week03/MovieList/MovieList/Movie.cs
+++ Week03/MovieList/MovieList/Movie.cs
@@ -5,6 +5,22 @@ namespace MovieList
 {
     public class Movie
     {
+        public string MovieName;
+        private int ReleaseYear;
+        private double RottenTomatoes;
 
+        public Movie (string name, int releaseYear, double rottenTomatoes)
+        {
+            this.MovieName = name;
+            this.ReleaseYear = releaseYear;
+            this.RottenTomatoes = rottenTomatoes;
+        }
+
+        public void ShowMovie()
+        {
+
+            
+            MessageBox.Show(MovieName + ReleaseYear);
+        }
     }
 }
